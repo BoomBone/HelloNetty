3b68f116de7f9844d5c38cbede65ebf1806f2e8f
Ting <ticccccc@163.com>
2018-09-29 16:56:47 +0800

    构建hello服务器

---
+++ src/main/java/com/imooc/netty/HelloServer.java
@@ -0,0 +1,38 @@
+package com.imooc.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 实现客户端发送一个请求，服务器返回hello netty
+ */
+public class HelloServer {
+    public static void main(String[] args) throws Exception {
+
+        //定义一队线程组，用于接受客户端的连接，不做任何处理
+        //主线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        //从线程组，老板线程组会把任务丢给他，让手下线程组去做任务
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try{
+            //netty服务器的创建，ServerBootstrap 是一个启动类
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(null);
+            //启动Server并且设置8088为启动的端口号
+            ChannelFuture channelFuture = serverBootstrap.bind(8088).sync();
+
+            //用于监听关闭的channel
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
