5f5555d28de4f6e83c3377607cf91bc79a237e2a
Ting <ticccccc@163.com>
2018-09-30 10:37:46 +0800

    hello服务器启动与讲解

--- src/main/java/com/imooc/netty/CustomHandler.java
+++ src/main/java/com/imooc/netty/CustomHandler.java
@@ -14,27 +14,32 @@ import io.netty.util.CharsetUtil;
 
 //SimpleInboundHandler：对于请求来讲，其实相当于[入栈，入境]
 public class CustomHandler extends SimpleChannelInboundHandler<HttpObject> {
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject)
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext,
+                                HttpObject httpObject)
             throws Exception {
         //获取channel
         Channel channel = channelHandlerContext.channel();
-        //显示客户端的远程地址
-        System.out.println(channel.remoteAddress());
 
-        //定义发送的数据消息
-        ByteBuf content = Unpooled.copiedBuffer("Hello,Netty!", CharsetUtil.UTF_8);
+        if (httpObject instanceof HttpRequest) {
 
-        //构建一个http响应response
-        DefaultFullHttpResponse response =
-                new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
-                        HttpResponseStatus.OK, content);
+            //显示客户端的远程地址
+            System.out.println(channel.remoteAddress());
 
-        //为响应增加数据类型和长度
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            //定义发送的数据消息
+            ByteBuf content = Unpooled.copiedBuffer("Hello,Netty!", CharsetUtil.UTF_8);
 
-        //把响应写到客户端
-        channelHandlerContext.writeAndFlush(response);
+            //构建一个http响应response
+            DefaultFullHttpResponse response =
+                    new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                            HttpResponseStatus.OK, content);
 
+            //为响应增加数据类型和长度
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+            //把响应写到客户端
+            channelHandlerContext.writeAndFlush(response);
+
+        }
     }
 }
